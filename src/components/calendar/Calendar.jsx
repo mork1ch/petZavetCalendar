978f4c36ae76a205c417d4273c34bc9355bd5431
Mork1ch <mork1cher@gmail.com>
2022-07-14 12:59:09 +0300

    test moment Library

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,41 +1,88 @@
+import moment from "moment";
 import React from "react";
 import "./style/calendar.scss";
 
 export default function Calendar() {
 	let date = new Date();
-	console.log(date);
+	// console.log(date);
 
-	const year = date.getFullYear(),
-		month = "Apr",
-		monthFull = "April",
-		dayN = date.getDate(),
-		dayW = "Fri";
+	var months = [
+			"Январь",
+			"Февраль",
+			"Март",
+			"Апрель",
+			"Май",
+			"Июнь",
+			"Июль",
+			"Август",
+			"Сентябрь",
+			"Октябрь",
+			"Ноябрь",
+			"Декабрь",
+		],
+		shortMonths = [
+			"Янв",
+			"Февр",
+			"Март",
+			"Апр",
+			"Май",
+			"Июнь",
+			"Июль",
+			"Авг",
+			"Сент",
+			"Окт",
+			"Нояб",
+			"Дек",
+		],
+		days = [
+			"Воскресенье",
+			"Понедельник",
+			"Вторник",
+			"Среда",
+			"Четверг",
+			"Пятница",
+			"Суббота",
+		];
+
+	var testDate = moment("MMMM");
+	console.log("test", testDate);
+
+	// Дата сегодня
+	const yearNow = date.getFullYear(),
+		monthNow = shortMonths[date.getMonth()],
+		monthNowFull = months[date.getMonth()],
+		dayNowNumber = date.getDate(),
+		dayNowText = days[date.getDay()];
+
+	var daysOnMonth = new Date(yearNow, date.getMonth(), 0).getDate();
+	console.log(daysOnMonth);
 
 	return (
 		<div className="calendar">
 			<div className="title">
-				<p className="year">{year}</p>
+				<p className="yearNow">{yearNow}</p>
 				<h2 className="datePicked">
-					{dayW}, {month} {dayN}
+					{dayNowText}, {monthNow} {dayNowNumber}
 				</h2>
 			</div>
 			<div className="bottom">
 				<div className="setMonth">
 					<button className="arrLeft"></button>
 					<p className="month">
-						{monthFull} {year}
+						{monthNowFull} {yearNow}
 					</p>
 					<button className="arrRight"></button>
 				</div>
 				<div className="dayOfWeek">
-					<p>s</p>
-					<p>m</p>
-					<p>t</p>
-					<p>w</p>
-					<p>t</p>
-					<p>f</p>
-					<p>s</p>
+					<p>п</p>
+					<p>в</p>
+					<p>с</p>
+					<p>ч</p>
+					<p>п</p>
+					<p>с</p>
+					<p>в</p>
 				</div>
+				<div className="days"></div>
 			</div>
 		</div>
 	);
