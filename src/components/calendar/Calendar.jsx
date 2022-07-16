dbfa8bebf7b0d7163929eeb616a7b4077ae1b975
Mork1ch <mork1cher@gmail.com>
2022-07-16 19:18:10 +0300

    s

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,4 +1,3 @@
-import moment from "moment";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { monthSetNext, monthSetPrev } from "../../store/dateReducer";
